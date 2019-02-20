bdd7472847970bd52dec6b72ef1c94d078b39134
avatar4ik3 <47820118+avatar4ik3@users.noreply.github.com>
2019-02-20 20:04:00 +0600

    Add files via upload

--- IDZ 1_2.cpp
+++ IDZ 1_2.cpp
@@ -6,44 +6,12 @@
 #include <fstream>
 #include <stdlib.h>
 #include <string>
+#include "class.h"
 #define _CRT_SECURE_NO_WARNINGS
 using namespace std;
-int file_check(string name) {
-	if (name.find_last_of(".") == -1 || name.substr(name.find_last_of(".") + 1) != "txt")return 2;
-	FILE *in = fopen(name.c_str(), "w");
-	if (in == NULL) {
-		return 2;
-	}
-		else {
-			fclose(in);
-			return 1;
-		}
-	fclose(in);
-	return 0;
-}
-/*int calculation_max(int &a) {
-	int max = -100;
-	for (int i = 0; i < 3; i++) {
-		if (a > max) {
-			max = a;
-		}
-	}
-	return max;
-}*/
+
 int main()
 {
-	int a, b;
-	string name1 = "C:\\Users\\asbox\\source\\repos\\File_generator\\File_generator";
-	string name2;
-	cin >> name2;
-	if (file_check(name2) == 2) {
-		cout << "Ошибка работы с файлом";
-		system("pause");
-		return 0;
-	}
-	ifstream file;
-	file.open(name1 + name2);
-	getline(file, name1);
-	file.close();
-	system("pause");
+	Semi a;
+	cout << a.summa(2, 3);
 }
---
+++ class.h
@@ -0,0 +1,8 @@
+#pragma 
+//seminar ot genia denisa
+class Semi {
+	public:
+		int summa(int a, int b);
+	private:
+
+};
